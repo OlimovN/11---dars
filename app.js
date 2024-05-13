9048b9be96f2b6c3761102dbf7e0559327bc9eaf
OlimovN <olimonurmuhammad711@gmail.com>
2024-05-13 06:38:54 +0500

    11 - dars

---
+++ app.js
@@ -0,0 +1,123 @@
+// 1 - masala
+
+// function teskari ((string) => {
+//    const = 
+// }); 
+
+
+//  2 - masala
+
+let a = 5
+// let b = 5;
+
+
+// if (a == b) {
+//     console.log("Birinchi shart ");
+// }else{
+//     console.log("Ikkinchi  shart ");
+    
+// }
+ 
+
+// if (a === b && a === b) {
+//   console.log("Birinchi shart ");
+// } else {
+//   console.log("Ikkinchi  shart ");
+// }
+
+
+
+// function cmdArray(a, array = []) {
+//     if (a <= 0) {
+//       return array;
+//     } else {
+//       array.unshift(a);
+//       return createArray(a - 1, array);
+//     }
+//   }
+
+
+//   const a = 5;
+//   const resultArray = cmdArray(a);
+//   console.log(resultArray); 
+
+
+
+// const numbers = [10, 7, 25, 30, 42, 11]; 
+
+// const result = numbers.reduce((acc, curr) => {
+//   if (curr % 2 === 0) {
+//     acc[1] += curr; 
+//   } else {
+//     acc[0] += curr; 
+//   }
+//   return acc;
+// }, [0, 0]);
+
+// console.log(result); 
+
+
+
+// function removeExclamationMarks(str) {
+//     return str.replace(/!/g, '');
+// }
+
+// let text = "Bu ! bir ! misol ! matn !";
+// let newText = removeExclamationMarks(text);
+// console.log(newText);
+
+
+
+// function yigindi(array1, array2) {
+//     let sum = 0;
+
+//     for (let num of array1) {
+//         sum += num;
+//     }
+
+//     for (let num of array2) {
+//         sum += num;
+//     }
+
+//     return sum;
+// }
+
+
+
+// function removeFirstAndLast(str) {
+//     return str.slice(1, -1);
+// }
+
+
+// function reverseArray(arr) {
+//     return arr.reverse();
+// }
+
+// let array = [1, 2, 3, 4, 5];
+// console.log(reverseArray(array)); // Natija: [5, 4, 3, 2, 1]
+
+
+
+
+// function ochiribTashlash(string) {
+//     if (string.endsWith('0')) {
+//       return string.slice(0, -1);
+//     }
+//     return string;
+//   }
+
+//   console.log(ochiribTashlash("12340")); 
+//   console.log(ochiribTashlash("56789")); 
+
+
+
+
+// function engKichikSon(array) {
+//     let min = Math.min(...array);
+//     return min ** 4 + "";
+//   }
+  
+//
+//   let numbers = [3, 7, 2, 9, 5];
+//   console.log(engKichikSon(numbers)); 
+
